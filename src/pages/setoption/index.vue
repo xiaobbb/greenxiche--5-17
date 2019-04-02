83958a3f173de168b0f9072b854976280f2fdc49
dongdong <928150957@qq.com>
2019-04-02 19:04:33 +0800

    完成地址管理，支付密码修改。意见反馈差上传

--- src/pages/setoption/index.vue
+++ src/pages/setoption/index.vue
@@ -2,25 +2,30 @@
   <div class="backgray setlist">
       <div class="commonpad white">
           <div>
-              <textarea name="" id="" cols="20" rows="5" placeholder="请输入您的反馈问题及建议..."></textarea>
-              <p class="nums">0/20</p>
+              <textarea name="" id=""  maxlength="120" v-model="text" placeholder="请输入您的反馈问题及建议..."></textarea>
+              <p class="nums">{{text.length}}/120</p>
           </div>
       </div>
       <div class="relate white commonpad ">
           <div class="flex-container comtitile">
               <p>相关截图（选填）</p>
-              <p class="nums">0/4</p>
+              <p class="nums">{{imgBase.length}}/4</p>
           </div>
-          <p class="flex-container">
-              <img src="/static/images/bg20.png" class="compics">
-          </p>
+          <div class="imgs" >
+              <div class="imgItem" v-for="(img,index) in imgBase" :key="index">
+                <div class="delete"  @click="deleteImg(index)">×</div>
+               <img :src="img.PicUrl" alt="">
+              </div>
+              <img src="/static/images/bg20.png" @click="chosseImg" v-show="!(imgBase.length >3)" class="compics">
+          </div>
       </div>
-      <div class="paybtn setbtn">确定</div>
+      <div class="paybtn setbtn" @click="submit">确定</div>
       
   </div>
 </template>
 
 <script>
+import {post} from "@/utils/index";
 import "../../css/common.css";
 import "../../css/global.css";
 export default {
@@ -29,7 +34,10 @@ export default {
   },
   data () {
     return {
-      
+      text:'',
+      imgBase:[],
+      userid: wx.getStorageSync("userid"),
+      token: wx.getStorageSync("token"),
     }
   },
  
@@ -42,8 +50,56 @@ export default {
         title: "意见反馈"
       });
     },
-    
-    
+    // 修改图片
+    chosseImg() {
+      const that = this;
+      wx.chooseImage({
+        success: res => {
+          wx.getFileSystemManager().readFile({
+            filePath: res.tempFilePaths[0], //选择图片返回的相对路径
+            encoding: "base64", //编码格式
+            success: ress => {
+              //成功的回调
+              that.imgBase.push({
+                PicUrl:'data:image/png;base64,'+ress.data.toString()
+              });
+            }
+          });
+          
+          //以下两行注释的是同步方法 转化后的base路径
+          // that.imgBase = wx .getFileSystemManager().readFileSync(res.tempFilePaths[0], "base64");
+          // console.log("that.imgBase" + that.imgBase);
+          // this.updateimg();
+        }
+      });
+    },
+    async submit() {
+      console.log(JSON.stringify(this.imgBase))
+      if(!this.text){
+        wx.showToast({
+          title:'请输入反馈内容！',
+          icon: "none"
+        });
+        return false;
+      }
+      const result = await post("User/MemberFeedBack", {
+        UserId: this.userid,
+        Token: this.token,
+        Content: this.text,
+        PicList: JSON.stringify(this.imgBase),
+        Type:''
+      });
+        wx.showToast({
+          title:result.msg,
+          icon: "success"
+        });
+        wx.navigateBack();
+    },
+    deleteImg(i){
+      this.imgBase.splice(i,1)
+      this.deleteImgIndex=''
+      console.log(this.deleteImgIndex)
+    },
   },
 
   created () {
@@ -55,4 +111,31 @@ export default {
 <style lang="scss" scoped>
   @import "./style";
   @import "../../css/common.css";
+  .imgs{
+    display:flex;
+    align-items:center;
+    flex-flow:row wrap;
+    .imgItem{
+    position:relative;
+    }
+    img {
+      width:156rpx;
+      height:156rpx;
+      margin-right:10rpx;
+      margin-bottom:20rpx;
+    }
+    .delete{
+      width:36rpx;
+      height:36rpx;
+      line-height:30rpx;
+      background:rgba(0,0,0,.6);
+      color:#fff;
+      font-size:34rpx;
+      text-align:center;
+      border-radius:50%;
+      position:absolute;
+      top:-15rpx;
+      right:0;
+    }
+  }
 </style>
--- src/pages/sitemanage/index.vue
+++ src/pages/sitemanage/index.vue
@@ -1,110 +1,133 @@
 <template>
   <div class="backgray">
-      <div class="carlist">
-          <radio-group class="radio-group"  @change="radioChange">
-            <div  v-for="item in carinfolist" :key="item.id">
-                <div class="caritem white commonpad">
-                  <div class="flex-container person">
-                      <p>{{item.name}}</p>
-                      <p class="phonenum">{{item.phone}}</p>
-                  </div>
-                  <div>{{item.site}}</div>
-                  <div class="flex-container choosemenu">
-                    <label class="radio flex-container choose">
-                        <radio :value="item.id" :checked="item.checked"/>
-                        <span class="chosetitle" v-if="!item.checked">默认地址</span>
-                        <span class="chosetitle2" v-else>设为默认地址</span>
-                    </label>
-                    <div class="flex-container edit">
-                        <p>
-                            <img src="/static/images/edit.png" class="menu">
-                            <text>编辑</text>
-                        </p>
-                        <p  @click="deleteSite">
-                            <img src="/static/images/del.png" class="menu">
-                            <text>删除</text>
-                        </p>
-                    </div>
-                  </div>
+    <div class="carlist">
+      <radio-group class="radio-group" @change="radioChange">
+        <div v-for="(item,index) in carinfolist" :key="item.id">
+          <div class="caritem white commonpad">
+            <div class="flex-container person">
+              <p>{{item.name}}</p>
+              <p class="phonenum">{{item.phone}}</p>
+            </div>
+            <div>{{item.site}}</div>
+            <div class="flex-container choosemenu">
+              <label class="radio flex-container choose" @click="changeDefault(index)">
+                <radio :value="item.id" :checked="item.checked"/>
+                <span class="chosetitle" v-if="!item.checked">默认地址</span>
+                <span class="chosetitle2" v-else>设为默认地址</span>
+              </label>
+              <div class="flex-container edit"  @click="editAddress(item.id)" >
+                <p>
+                  <img src="/static/images/edit.png" class="menu">
+                  <text>编辑</text>
+                </p>
+                <p @click="deleteSite(item.id,index)">
+                  <img src="/static/images/del.png" class="menu">
+                  <text>删除</text>
+                </p>
               </div>
             </div>
-          </radio-group>
-      </div>
-      <!--底部按钮-->
-      <div class="btn" @click="addSite">
-          <div>
-              <img src="/static/images/add.png" class="add">
-              <span>添加新地址</span>
           </div>
+        </div>
+      </radio-group>
+    </div>
+    <!--底部按钮-->
+    <div class="btn" @click="addSite">
+      <div>
+        <img src="/static/images/add.png" class="add">
+        <span>添加新地址</span>
       </div>
+    </div>
     <!--弹框遮罩-->
     <div class="mask-modal" v-if="isshow"></div>
     <div class="mask white flex-container clomn" v-if="isshow">
-        <p class="btntitle">您确定删除该收货地址吗？</p>
-        <p class="flex-container buttontbn">
-            <text @click="cancle">取消</text>
-            <text>确定</text>
-        </p>
+      <p class="btntitle">您确定删除该收货地址吗？</p>
+      <p class="flex-container buttontbn">
+        <text @click="cancle">取消</text>
+        <text>确定</text>
+      </p>
     </div>
+    <van-dialog id="van-dialog"/>
   </div>
 </template>
 
 <script>
-import {post} from '@/utils/index'
+import { post } from "@/utils/index";
+import Dialog from "../../../static/vant/dialog/dialog";
 import "../../css/common.css";
 import "../../css/global.css";
 export default {
-  data () {
+  data() {
     return {
-      
-        userid: wx.getStorageSync('userid'),
-        token: wx.getStorageSync('token'),
-        isshow:false,
-        data:0,
-        carinfolist:[
-          {id:1,name:"张三",phone:"13500232365",site:"广东省 深圳市 龙岗区 坂田街道 中心路十里花园5期12 栋404室",checked:true},
-          {id:2,name:"张三",phone:"13652145698",site:"广东省 深圳市 龙岗区 坂田街道 中心路十里花园5期12 栋404室",checked:false}
-          
-        ]
-    }
+      userid: wx.getStorageSync("userid"),
+      token: wx.getStorageSync("token"),
+      isshow: false,
+      data: 0,
+      carinfolist: []
+    };
   },
- 
-  components: {
-    
-  },
-  mounted(){
-      wx.setNavigationBarTitle({
-        title: '地址管理'
-      });
-      this.getData();
+
+  components: {},
+  mounted() {
+    wx.setNavigationBarTitle({
+      title: "地址管理"
+    });
+    this.getData();
   },
   methods: {
-    async getData(){
-      const params ={
+    async getData() {
+      const params = {
         UserId: this.userid,
         Token: this.token,
-        Page:1,
-        PageSize:20
-      }
-      const res =await post('Address/AddressList',params)
-      for(let i=0;i<res.data.length;i+=1){
-        const list = res.data[i]
+        Page: 1,
+        PageSize: 20
+      };
+      const res = await post("Address/AddressList", params);
+      
+      this.carinfolist= []
+      for (let i = 0; i < res.data.length; i += 1) {
+        const list = res.data[i];
         this.carinfolist.push({
-          id:list.id,
-          name:list.name,
-          phone:list.tel,
-          site:list.address,
-          checked:list.is_def
-        })
+          id: list.id,
+          name: list.name,
+          phone: list.tel,
+          site: list.addressinfo,
+          checked: list.is_def
+        });
       }
     },
-    deleteSite(){
-       this.isshow=true
-     },
-    cancle(){
-      this.isshow=false
+    deleteSite(id, i) {
+      const that = this;
+      //  this.isshow=true
+      Dialog.confirm({
+        title: "您确定删除该收货地址吗？",
+        message: ""
+      })
+        .then(() => {
+          post("Address/DeleteAddress", {
+            Id: id,
+            UserId: this.userid,
+            Token: this.token
+          }).then(res => {
+            that.carinfolist.splice(i, 1);
+          });
+          // on confirm
+        })
+        .catch(() => {
+          // on cancel
+        });
     },
-    radioChange(e){
+    changeDefault(i){
+        const params={
+            Id: this.carinfolist[i].id,
+            UserId: this.userid,
+            Token: this.token
+        }
+        post("Address/SetDefaultaddress",params)
+    },
+    cancle() {
+      this.isshow = false;
+    },
+    radioChange(e) {
       //console.log(e.target.value)
       var arrs = this.carinfolist;
       var that = this;
@@ -115,21 +138,22 @@ export default {
           arrs[x].checked = false;
         }
       }
-     
     },
-    addSite(){
-      console.log(666)
-      wx.navigateTo({ url: "/pages/addnewsite/main" });
+    addSite() {
+      wx.redirectTo({ url: "/pages/addnewsite/main" });
     },
+    editAddress(id){
+      wx.redirectTo({ url: "/pages/addnewsite/main?id="+id});
+    }
   },
 
-  created () {
+  created() {
     // let app = getApp()
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-  @import "./style";
-  @import "../../css/common.css";
+@import "./style";
+@import "../../css/common.css";
 </style>
