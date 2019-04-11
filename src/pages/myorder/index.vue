814679563f60b5bf4c44397f94d2642f4c18d621
dongdong <you@example.com>
2019-04-11 19:24:23 +0800

    做了商品评价功能

--- src/pages/myorder/index.vue
+++ src/pages/myorder/index.vue
@@ -2,15 +2,15 @@
   <div class="backgray">
     <div class="menuTypeBox">
       <div class="menuType">
-        <span class="item" :class="{'active':orderBigType===0}" @click="shiftOrderBigType(0)">商城订单</span>
-        <span class="item" :class="{'active':orderBigType===1}" @click="shiftOrderBigType(1)">预约订单</span>
+        <span class="item" :class="{'active':orderBigType===1}" @click="shiftOrderBigType(1)">商城订单</span>
+        <span class="item" :class="{'active':orderBigType===2}" @click="shiftOrderBigType(2)">预约订单</span>
       </div>
     </div>
     <div class="menuContent">
       <!--商城订单-->
-      <div v-if="orderBigType===0">
+      <div v-if="orderBigType===1">
         <div class="menuhead flex-container white">
-          <p v-for="item in menulist" :key="item.id" :class="{active:active==item.id}" @click="shiftStatus(item.id)">{{item.name}}</p>
+          <p v-for="item in menulist" :key="item.id" :class="{active:status==item.id}" @click="shiftStatus(item.id)">{{item.name}}</p>
         </div>
         <div class="shoplist">
           <div>
@@ -22,7 +22,7 @@
                 <p class="tips">{{item.StatusName}}</p>
               </div>
               <div class="bgindo">
-                <div class="proinfo flex-container pad" v-for="(item2,index2) in item.orderDetails" :key="index2">
+                <div class="proinfo flex-container pad" v-for="(item2,index2) in item.orderDetails" :key="index2" @click="toOrderDetail(1,item2.OrderNumber)">
                   <div>
                     <img :src="item2.ProductImg" class="orderimg">
                   </div>
@@ -60,7 +60,7 @@
         </div>
       </div>
       <!--预约订单-->
-      <div v-if="orderBigType===1">
+      <div v-if="orderBigType===2">
         <div class="menuhead flex-container white visit">
           <div class="item flex1 center" v-for="item in visitlist"
             :key="item.id"
@@ -79,15 +79,19 @@
               <p class="tips">{{item.StatusName}}</p>
             </div>
             <!--切换上门服务-->
-            <div class="seritem" v-if="active==1">
-              <p>项目：{{}}</p>
-              <p>地址：广东省深圳市福田区福华三路116号</p>
-              <p>车辆：别克-凯越 红色 粤AH6688</p>
+            <div class="seritem">
+              <p>项目：{{item.ProductName}}</p>
+              <p>地址：{{item.ServiceAddr}}</p>
+              <p>车辆：<span style="margin-right:10rpx;">{{item.CarBrand}}<span v-if="item.CarType">-{{item.CarType}}</span></span><span style="margin-right:10rpx;" v-if="item.CarColor">{{item.CarColor}}</span><span style="margin-right:10rpx;">{{item.CarMumber}}</span></p>
               <p>时间：2019-03-18 14:00-16:00</p>
               <p>手机号：13682293390</p>
             </div>
+            <div class="menubtn flex-container flexEnd">
+              <text class="btn">取消订单</text>
+              <text class="btn active">支付</text>
+            </div>
             <!--切换到店服务-->
-            <div class="seritem" v-if="active==2">
+            <!-- <div class="seritem" v-if="active==2">
               <p>项目：外观简单清洗洗车</p>
               <p>门店：车御品汽车服务</p>
               <p>车辆：别克-凯越 红色 粤AH6688</p>
@@ -96,7 +100,7 @@
             <div class="menubtn flex-container flexEnd">
               <text class="btn">取消订单</text>
               <text class="btn active">付款</text>
-            </div>
+            </div> -->
           </div>
           <p style="text-align:center;font-size:30rpx;color:#666;padding:120rpx 20rpx 80rpx;" v-if="hasData">暂无数据</p>
           <p class="ovedMsg" v-if="isOved" style="text-align:center;padding:20rpx;font-size:26rpx;color:#666;">我也是有底线的</p> 
@@ -114,13 +118,41 @@ import "../../css/global.css";
 export default {
   onLoad() {
     this.setBarTitle();
+    // orderNo=201904111016183668853&appraiseType=0&appraiseId=1120,==>最终是要传到评价页面的，appriseType: 0:商城产品评价；1：上门评价；2：到店评价sd
+    //跳转到评价的时候，首先要判断一个订单有多少条数据，如果有超过一条的话，就要有跳转到评价列表里面，从评价列表中去一条一条的评价,
+    //
   },
   onShow() {
     this.userId = wx.getStorageSync("userId");
     this.token = wx.getStorageSync("token");
     this.orderList = [];
     this.bookList = [];
-    this.getOrderList();
+    if(this.$root.$mp.query.orderBigType){  //是商城订单还是预约订单;1:商城订单；2：预约订单
+      this.orderBigType = this.$root.$mp.query.orderBigType;
+    }else{
+      this.orderBigType = 1;
+    }
+    if(this.orderBigType===1){
+      if(this.$root.$mp.query.status || this.$root.$mp.query.status==0){
+        this.status = this.$root.$mp.query.status;
+      }else{
+        this.status = 0;
+      }
+      this.serviceMode = 1;
+      this.getOrderList();
+    }
+    if(this.orderBigType===2){
+      if(this.$root.$mp.query.status){
+        this.serviceMode = this.$root.$mp.query.status;
+      }else{
+        this.serviceMode = 1;
+      }
+      this.status = 0;
+      this.getReserveOrderList();
+    }
+    console.log("orderBigType:"+this.orderBigType);
+    console.log("status:"+this.status);
+    console.log("serviceMode:"+this.serviceMode);  
   },
   data() {
     return {
@@ -134,9 +166,7 @@ export default {
       isLoad:false,
       isOved:false,
       hasData:false,
-      orderBigType: 0,
-      active: "0",
-      showButton: true,
+      orderBigType: 1,  //1:商城订单；2：预约订单
       serviceMode:1,
       menulist: [
         { id: 0, name: "全部" },
@@ -163,20 +193,22 @@ export default {
     shiftOrderBigType(e) {
       this.initData();
       this.orderBigType = e;
-     if(this.orderBigType===0){
+     if(this.orderBigType===1){
        this.getOrderList();
      }
-     if(this.orderBigType===1){
+     if(this.orderBigType===2){
         this.getReserveOrderList();
      }
     },
     change(e) {
       //console.log(e)
+      this.initData();
       this.serviceMode = e;
+      this.getReserveOrderList();
     },
-    toOrderDetail(e) {
+    toOrderDetail(e,orderNo) {
       if (e == 1) {
-        wx.navigateTo({ url: "/pages/orderdetail/main" });
+        wx.navigateTo({ url: "/pages/orderdetail/main?orderNo="+orderNo });
       }
       if (e == 2) {
         wx.navigateTo({ url: "/pages/appointorderdet/main" });
@@ -192,12 +224,8 @@ export default {
       this.orderList = [];
       this.bookList = [];
     },
-    getOrderList(e){
-      
-    },
     shiftStatus(status){
       this.status = status;
-      this.active = status;
       this.initData();
       this.getOrderList();
     },
@@ -256,7 +284,8 @@ export default {
       if(result.data.length>0){
         this.bookList = this.bookList.concat(result.data);
       }
-    }
+    },
+
   },
 
   created() {
