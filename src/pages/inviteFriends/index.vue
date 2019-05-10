a0571f26a7213beda775939dafa584bc44e600e9
Ans_xu <835858556@qq.com>
2019-05-10 14:44:18 +0800

    完成会员登录，邀请好友页面切图

--- src/pages/inviteFriends/index.vue
+++ src/pages/inviteFriends/index.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    
+  <div class="boxs" >
+    <div class="bgimg">
+      <img src="/static/images/friends-bg.jpg" alt="">
+    </div>
+    <div class="content">
+    <div class="obtain p40">
+        <div class="flexs-align top">
+          <div class="item">
+            <div class="number">10</div>
+            <p>积分</p>
+          </div>
+          <div class="b2-eae2df"></div>
+          <div class="item">
+            <div class="number">20</div>
+            <p>服务卡券</p>
+          </div>
+        </div> 
+        <div class="checked">查看</div>
+    </div>
+    <div class="wx">
+      <img src="/static/images/friend.png" alt="">
+      <p>微信邀请</p>
+    </div>
+    <div class="rule">
+      <h2>推荐规则</h2>
+      <p v-for="(item,index) in rule" :key="index">{{index+1}}、{{item}}</p>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -16,7 +42,11 @@ export default {
   },
   data() {
     return {
-
+      rule:[
+        '每成功推荐1位好友注册，好友和推荐人同时获得积分 或服务卡券。',
+        '推荐的好友需通过推荐人的邀请码、二维码、发送的链 接，才可建立推荐关系。',
+        '若发现恶意注册推荐好友获得奖励，平台有权取消该奖励。'
+      ]
     }
   },
   methods:{
--- src/pages/vipGrade/index.vue
+++ src/pages/vipGrade/index.vue
@@ -1,27 +1,47 @@
 <template>
   <div>
     <!-- 用户信息 -->
-    <div class="userInfo">
-          <div class="flex-container clomn avahead">
+    <div class="userInfo flexs-align">
               <img v-if="memberInfo.Avatar" :src="memberInfo.Avatar" class="avatar">
               <img v-else src="/static/images/default.png" class="avatar">
-              <img src="/static/images/default.png" class="avatar">
-              <div class="flexs name" @click="gotoVipGrade">
+              <div class="ml30">
+                 <div class="flexs-align name">
                 <p class="person">{{memberInfo.NickName}}</p>
                 <img src="/static/images/vip_v.png" alt="">
                 <p class="level">LV0</p>
               </div>
               
-              <!-- <div class="person_id">ID:{{memberInfo.MemberID}}</div> -->
+              <div class="person_id">ID:{{memberInfo.MemberID}}</div>
+              </div>
+             
+         
+    </div>
+    <div class="plr30">
+      <!-- 等级对照表 -->
+      <div class="grade-tabel">
+        <h3>等级对照表</h3>
+        <p>会员升级与购买产品直接相关。</p>
+        <p>不同会员的等级如下图所示：</p>
+        <div class="tabel">
+          <div class="head flexs-align">
+            <div class="left">会员等级</div>
+            <div class="right">升级条件</div>
           </div>
-    </div>
-    <!-- 等级对照表 -->
-    <div class="grade-tabel">
-
-    </div>
-    <!-- 权益介绍 -->
-    <div class="introduction">
-
+          <div class="bodys">
+            <div class="item flexs-align" v-for="(item,index) in gradeTabel" :key="index">
+              <div class="left">{{item.level}}</div>
+              <div class="right flexs-align" @click="gotoProduct">{{item.content}} <van-icon name="arrow" class="mt8"></van-icon>  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <!-- 权益介绍 -->
+      <div class="introduction">
+        <h3>权益介绍</h3>
+        <p>会员体系是为了给处于不同阶段的用户提供更贴心，
+更强大的服务，以满足不同阶段用户的需求。不同等级
+的VIP会员享受不同的优惠权益，级别越高，优惠越大。</p>
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +51,29 @@ import { post } from "../../utils";
 import "../../css/common.css";
 import "../../css/global.css";
 export default {
+  data() {
+    return {
+      memberInfo:{},
+      gradeTabel:[
+        {
+          level:'LV0',
+          content:'购买绿妞长效镀晶普通版'
+        },
+        {
+          level:'LV1',
+          content:'购买绿妞长效镀晶经典版'
+        },
+        {
+          level:'LV2',
+          content:'购买绿妞长效镀晶旗舰版'
+        },
+        {
+          level:'LV3',
+          content:'购买绿妞长效镀晶至尊版'
+        },
+      ]
+    }
+  },
   onLoad() {
     this.setBarTitle();
   },
@@ -38,19 +81,14 @@ export default {
     console.log('userInfo',wx.getStorageSync('userInfo'))
     this.memberInfo= wx.getStorageSync('userInfo')
   },
-  data() {
-    return {
-      memberInfo:{}
-    }
-  },
   methods:{
     setBarTitle(){
       wx.setNavigationBarTitle({
         title: "会员等级"
       });
     },
-    gotoVipGrade(){
-      
+    gotoProduct(){
+
     }
   },
   created() {}
